33ca6b7344ac8177cb62f980214c0af61acda3fa
Jonathan Eberle <jeberle@2-B-7.42heilbronn.de>
2026-02-19 20:40:24 +0100

    basic parser - not finished clean

--- obj/main.d
+++
@@ -1,5 +0,0 @@
-obj/main.o: src/main.c includes/ping.h libft/libft.h
-
-includes/ping.h:
-
-libft/libft.h:
--- obj/parser/parser.d
+++
@@ -1,5 +0,0 @@
-obj/parser/parser.o: src/parser/parser.c includes/ping.h libft/libft.h
-
-includes/ping.h:
-
-libft/libft.h:
